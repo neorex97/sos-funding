ca48d01fc903009039208d2d02edc5850b00bde5
neorex97 <neorex97@protonmail.com>
2020-05-13 11:39:12 +0530

    second commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    
+    <ExpectedFunding/>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import ExpectedFunding from '@/components/ExpectedFunding.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ExpectedFunding,
+   
+  }
+}
+</script>
